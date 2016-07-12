70937ab27ccf44bdf84e59825643e7b03777bf64
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-12 16:32:23 +0200

    services added to bootstrap

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -3,8 +3,12 @@ import {HTTP_PROVIDERS} from '@angular/http';
 import {ionicBootstrap} from 'ionic-angular';
 
 import {AppComponent} from './components/app/app.component'
+import {TimetrackerApiReuqestService} from './services/api/timetracker/timetracker-api-request.service';
+import {SessionService} from './services/session/session.service';
 
 ionicBootstrap(
-    AppComponent, [
-        HTTP_PROVIDERS
-    ]);
+  AppComponent, [
+    HTTP_PROVIDERS,
+    SessionService,
+    TimetrackerApiReuqestService,
+  ]);
