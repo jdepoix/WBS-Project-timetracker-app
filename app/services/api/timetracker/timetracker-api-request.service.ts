138a9bb65865de0f4bb2cc5fe1d20d403629ba44
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-12 16:32:45 +0200

    timetracker api service added

--- app/services/api/timetracker/timetracker-api-request.service.ts
+++ app/services/api/timetracker/timetracker-api-request.service.ts
@@ -1,13 +1,30 @@
 import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
 
-import {SessionService} from '../session/session.service';
+import {SessionService} from '../../session/session.service';
+
+import {ApiRequest} from '../core/api-request';
+import {Url} from '../../../core/url/url';
 
 
 /**
  * takes care of handling HTTP requests to the timetracker backend. It set's authentication and headers.
  */
 @Injectable()
-export class TimetrackerApiReuqest {
-    constructor(private sessionService: SessionService) {
-    }
+export class TimetrackerApiReuqestService extends ApiRequest {
+  constructor(_http: Http, private _sessionService: SessionService) {
+    super(_http);
+  }
+
+  public getBaseUrl(): Url {
+    return this._sessionService.apiUrl.getRelativeUrl('api');
+  }
+
+  public getHeaders(): Headers {
+    let headers: Headers = super.getHeaders();
+
+    headers.append('Authorization', `Token ${this._sessionService.authenticationKey}`);
+
+    return headers
+  }
 }
