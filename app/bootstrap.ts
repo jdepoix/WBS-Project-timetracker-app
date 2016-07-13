fef279779320af0c83be9447f37ed942c2125642
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-13 11:44:05 +0200

    bootstrapin new services

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -5,10 +5,14 @@ import {ionicBootstrap} from 'ionic-angular';
 import {AppComponent} from './components/app/app.component'
 import {TimetrackerApiReuqestService} from './services/api/timetracker/timetracker-api-request.service';
 import {SessionService} from './services/session/session.service';
+import {WorkpackageService} from './services/workpackages/workpackage.service';
+import {ProjectService} from './services/projects/project.service';
 
 ionicBootstrap(
   AppComponent, [
     HTTP_PROVIDERS,
     SessionService,
     TimetrackerApiReuqestService,
+    WorkpackageService,
+    ProjectService
   ]);
