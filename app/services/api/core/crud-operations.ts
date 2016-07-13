017beaa70774723c805072a3c3f966f9cbc9eb52
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-13 17:48:06 +0200

    options param removed from crud interface, since optional params do not have to be specified by the interface

--- app/services/api/core/crud-operations.ts
+++ app/services/api/core/crud-operations.ts
@@ -4,17 +4,17 @@ import {RestModel} from '../../../models/core/rest-model/rest-model';
 import {Createable, Updateable} from '../../../models/core/rest-model/model-operations';
 
 export interface GetOperation<Model extends RestModel> {
-  get(options?: any): Observable<Array<Model>>;
+  get(): Observable<Array<Model>>;
 }
 
 export interface CreateOperation<Model extends RestModel & Createable> {
-  create(element: Model, options?: any): Observable<Model>;
+  create(element: Model): Observable<Model>;
 }
 
 export interface UpdateOperation<Model extends RestModel & Updateable> {
-  update(element: Model, options?: any): Observable<Model>;
+  update(element: Model): Observable<Model>;
 }
 
 export interface DeleteOperation<Model extends RestModel> {
-  delete(element: Model, options?: any): Observable<Model>;
+  delete(element: Model): Observable<Model>;
 }
