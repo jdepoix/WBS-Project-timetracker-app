e0d91a5425f50b3d185ae51d20bdda56b7aff087
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-13 11:43:44 +0200

    project service added

--- app/services/projects/project.service.ts
+++ app/services/projects/project.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
 
+import {Url} from '../../core/url/url';
+
 import {SessionService} from '../session/session.service';
 import {TimetrackerApiReuqestService} from '../api/timetracker/timetracker-api-request.service';
 
 @Injectable()
-class WorkpackageService {
+export class ProjectService {
   constructor(private _timetrackerApiService: TimetrackerApiReuqestService, private _sessionService: SessionService) {
   }
 }
