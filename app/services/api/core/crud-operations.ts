83fe970d5fa9acc939ccd9e4a790f3e083bc6865
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-13 17:16:20 +0200

    added optional options parameter to crud operations

--- app/services/api/core/crud-operations.ts
+++ app/services/api/core/crud-operations.ts
@@ -4,17 +4,17 @@ import {RestModel} from '../../../models/core/rest-model/rest-model';
 import {Createable, Updateable} from '../../../models/core/rest-model/model-operations';
 
 export interface GetOperation<Model extends RestModel> {
-  get(): Observable<Array<Model>>;
+  get(options?: any): Observable<Array<Model>>;
 }
 
 export interface CreateOperation<Model extends RestModel & Createable> {
-  create(element: Model): Observable<Model>;
+  create(element: Model, options?: any): Observable<Model>;
 }
 
 export interface UpdateOperation<Model extends RestModel & Updateable> {
-  update(element: Model): Observable<Model>;
+  update(element: Model, options?: any): Observable<Model>;
 }
 
 export interface DeleteOperation<Model extends RestModel> {
-  delete(element: Model): Observable<Model>;
+  delete(element: Model, options?: any): Observable<Model>;
 }
