1569db16afc6dcf59069a43503119d15d4936e17
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-13 18:44:21 +0200

    booking service bootstraped

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -7,12 +7,14 @@ import {TimetrackerApiReuqestService} from './services/api/timetracker/timetrack
 import {SessionService} from './services/session/session.service';
 import {WorkpackageService} from './services/workpackages/workpackage.service';
 import {ProjectService} from './services/projects/project.service';
+import {BookingService} from './services/bookings/booking.service';
 
 ionicBootstrap(
   AppComponent, [
     HTTP_PROVIDERS,
     SessionService,
     TimetrackerApiReuqestService,
+    BookingService,
     WorkpackageService,
     ProjectService
   ]);
