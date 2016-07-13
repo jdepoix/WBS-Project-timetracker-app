00270494c06d59022c0f11684622f1d93837c650
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-13 19:10:43 +0200

    bookings service implemented

--- app/models/booking/booking-serializers.ts
+++ app/models/booking/booking-serializers.ts
@@ -3,7 +3,8 @@ import {
   UrlDeserializerField,
   StringDeserializerField,
   DateDeserializerField,
-  RestModelDeserializerField
+  RestModelDeserializerField,
+  FloatDeserializerField
 } from '../core/rest-model/serializer-fields';
 
 import {Booking} from './booking';
@@ -11,6 +12,7 @@ import {Booking} from './booking';
 export class BookingDeserializer extends RestModelDeserializer<Booking> {
   protected _deserializerFields: Array<RestModelDeserializerField<Object>> = [
     new UrlDeserializerField('self'),
+    new FloatDeserializerField('effort'),
     new StringDeserializerField('description'),
     new DateDeserializerField('date'),
     new UrlDeserializerField('workpackage')
--- app/services/bookings/booking.service.ts
+++ app/services/bookings/booking.service.ts
@@ -1,12 +1,14 @@
 import {Injectable} from '@angular/core';
+import {Response} from '@angular/http';
 
 import {Observable} from 'rxjs/Rx';
 
-import {Url} from '../../core/url/url';
+import {Url, RessourceUrl} from '../../core/url/url';
 
-import {Booking} from '../../models/booking/booking';
-import {BookingDeserializer} from '../../models/booking/booking-serializers';
 import {RestModelListDeserializer} from '../../models/core/rest-model/serializers';
+import {Booking} from '../../models/booking/booking';
+import {Workpackage} from '../../models/workpackage/workpackage';
+import {BookingDeserializer} from '../../models/booking/booking-serializers';
 
 import {TimetrackerApiReuqestService} from '../api/timetracker/timetracker-api-request.service';
 import {GetOperation, CreateOperation, UpdateOperation, DeleteOperation} from '../api/core/crud-operations';
@@ -22,14 +24,17 @@ export class BookingService implements
   private _endpoint: Url;
 
   constructor(private _timetrackerApiService: TimetrackerApiReuqestService, private _sessionService: SessionService) {
-    this._endpoint = this._sessionService.subProjetUrl.getRelativeUrl('workpackages');
+    this._endpoint = this._sessionService.subProjetUrl.getRelativeUrl('bookings');
   }
 
-  public get(isToplevelWorkpackage?: boolean): Observable<Array<Booking>> {
+  public get(date?: Date, workpackage?: Workpackage): Observable<Array<Booking>> {
     let url: Url = this._endpoint;
 
-    if (isToplevelWorkpackage != null) {
-      url = url.clone().addParam('toplevel_wp', isToplevelWorkpackage);
+    if (date != null) {
+      url = url.clone().addParam('date', date.toJSON().split('T')[0]);
+    }
+    if (workpackage != null) {
+      url = url.clone().addParam('workpackage_id', RessourceUrl.getPrimaryKey(workpackage.self));
     }
 
     return this._timetrackerApiService.getWithFullUrl(url).map((plainObjects) => {
@@ -41,19 +46,19 @@ export class BookingService implements
 
   public update(element: Booking): Observable<Booking> {
     return this._timetrackerApiService.patchWithFullUrl(element.self, element.getUpdateRequestData())
-      .map((plainObject) => {
-        return new BookingDeserializer(plainObject).deserialize();
-      });
+      .map(this._deserializePlainObject);
   }
 
   create(element: Booking): Observable<Booking> {
     return this._timetrackerApiService.postWithFullUrl(element.self, element.getCreateRequestData())
-      .map((plainObject) => {
-        return new BookingDeserializer(plainObject).deserialize();
-      });
+      .map(this._deserializePlainObject);
   }
 
-  delete(element: Booking): Observable<Booking> {
-    return undefined;
+  delete(element: Booking): Observable<Response> {
+    return this._timetrackerApiService.deleteWithFullUrl(element.self);
+  }
+
+  private _deserializePlainObject(plainObject: Object): Booking {
+    return new BookingDeserializer(plainObject).deserialize();
   }
 }
