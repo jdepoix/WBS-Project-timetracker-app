057101beb47df9dc599e8271ee5fd3e3e3b88065
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-18 16:05:39 +0200

    authentication moved to dedicated service to avoid circel injection in a more elegant way

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -8,12 +8,14 @@ import {SessionService} from './services/session/session.service';
 import {WorkpackageService} from './services/workpackages/workpackage.service';
 import {ProjectService} from './services/projects/project.service';
 import {BookingService} from './services/bookings/booking.service';
+import {SessionAuthenticationService} from './services/session/session-authentication.service';
 
 ionicBootstrap(
   AppComponent, [
     HTTP_PROVIDERS,
     SessionService,
     TimetrackerApiReuqestService,
+    SessionAuthenticationService,
     BookingService,
     WorkpackageService,
     ProjectService
