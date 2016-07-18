c0ec889de22ce5f88f8b0930812bd6b47325f5d6
dimitri.babakow <dimitribabakow@gmail.com>
2016-07-18 10:07:12 +0200

    Settings for CORS

--- gulpfile.js
+++ gulpfile.js
@@ -78,3 +78,26 @@ gulp.task('scripts', copyScripts);
 gulp.task('clean', function(){
   return del('www/build');
 });
+
+var replace = require('replace');
+var replaceFiles = ['./www/js/app.js'];
+
+gulp.task('add-proxy', function() {
+    return replace({
+        regex: "http:localhost:8000/api",
+        replacement: "http://localhost:8100/api",
+        paths: replaceFiles,
+        recursive: false,
+        silent: false,
+    });
+})
+
+gulp.task('remove-proxy', function() {
+    return replace({
+        regex: "http://localhost:8100/api",
+        replacement: "http:localhost:8000/api",
+        paths: replaceFiles,
+        recursive: false,
+        silent: false,
+    });
+})
