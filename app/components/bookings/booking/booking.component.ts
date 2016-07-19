1f3495a621d538e174c6a334db8d408d7660722e
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-20 00:12:55 +0200

    booking component view added

--- app/components/bookings/booking/booking.component.ts
+++ app/components/bookings/booking/booking.component.ts
@@ -2,9 +2,12 @@ import {Component, Input} from '@angular/core';
 
 import {Booking} from '../../../models/booking/booking';
 
+import {WorkdaysToHoursPipe} from '../../../pipes/workdays-to-hours.pipe';
+
 @Component({
   selector: 'booking',
-  templateUrl: 'build/components/bookings/booking/booking.component.html'
+  templateUrl: 'build/components/bookings/booking/booking.component.html',
+  pipes: [WorkdaysToHoursPipe]
 })
 export class BookingComponent {
   private _isLiveBooking: boolean = false;
