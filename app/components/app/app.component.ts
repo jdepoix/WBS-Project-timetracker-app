9e0270b6971ce37bfa72292d43cc6dab5d905666
dimitri.babakow <dimitribabakow@gmail.com>
2016-07-19 16:28:01 +0200

    Load projects and check selected projects

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -6,10 +6,15 @@ import {StatusBar} from 'ionic-native';
 import {BookingOverviewComponent} from '../booking/overview/booking-overview.component';
 import {WorkpackageOverviewComponent} from '../workpackage/overview/workpackage-overview.component';
 import {LoginComponent} from "../login/login.component";
+import {ProjectService} from "../../services/projects/project.service";
+import {SessionAuthenticationService} from "../../services/session/session-authentication.service";
+import {SessionService} from "../../services/session/session.service";
+import {Project} from "../../models/project/project";
+import {NgIf} from "@angular/common";
 
 
 @Component({
-  templateUrl: 'build/components/app/app.component.html'
+  templateUrl: 'build/components/app/app.component.html',
 })
 export class AppComponent {
   @ViewChild(Nav) nav: Nav;
@@ -17,19 +22,26 @@ export class AppComponent {
   // make HelloIonicPage the root (or first) page
   rootPage: any = LoginComponent;
   pages: Array<{title: string, component: any}>;
+  projects: Array<Project>;
+  shownProjects: boolean;
+  selectedProject: string;
 
   constructor(
     private platform: Platform,
-    private menu: MenuController
+    private menu: MenuController,
+    private authService: SessionAuthenticationService,
+    private sessionService: SessionService,
+    private projectService: ProjectService
   ) {
     this.initializeApp();
-
     // set our app's pages
     this.pages = [
-      { title: 'Login' , component: LoginComponent}, 
       { title: 'Bookings', component: BookingOverviewComponent },
-      { title: 'Workpackages', component: WorkpackageOverviewComponent }
+      { title: 'Workpackages', component: WorkpackageOverviewComponent },
+      { title: 'Logout', component: LoginComponent}
     ];
+
+    this.shownProjects = false;
   }
 
   initializeApp() {
@@ -46,7 +58,39 @@ export class AppComponent {
   openPage(page) {
     // close the menu when clicking a link from the menu
     this.menu.close();
+
+    if(page.title == 'Logout'){
+      this.authService.logout();
+    }
+
+    if(this.sessionService.authenticationKey != '') {
+      this.projectService.get().subscribe((tmp:Array<Project>) => {
+        this.projects = tmp;
+      });
+    }
+
     // navigate to the new page if it is not the current page
     this.nav.setRoot(page.component);
+  }
+
+  toggleProjects(){
+    this.shownProjects = !this.shownProjects;
+  }
+
+  isProjectsShown(): boolean{
+    return this.shownProjects;
+  }
+
+  selectProject(project){
+    if(this.selectedProject == project.db){
+      this.selectedProject = '';
+    }
+    else {
+      this.selectedProject = project.db;
+    }
+  }
+
+  getProjectSelected(): string{
+    return this.selectedProject;
   }
 }
