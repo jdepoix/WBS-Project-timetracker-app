8997c48f7ab7a1d8c135cdca1c24065bafe57be1
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-19 23:14:58 +0200

    workpackage detail view implemented

--- app/components/bookings/booking/booking.component.ts
+++ app/components/bookings/booking/booking.component.ts
@@ -1,9 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+import {Booking} from '../../../models/booking/booking';
 
 @Component({
+  selector: 'booking',
   templateUrl: 'build/components/bookings/booking/booking.component.html'
 })
 export class BookingComponent {
+  @Input()
+  public booking: Booking;
+
   constructor() {
   }
 }
