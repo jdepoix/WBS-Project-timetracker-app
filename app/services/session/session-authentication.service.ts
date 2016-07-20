891aeaa0ba602a4bd9c7216f2fae9554d39dbbc7
Jonas Depoix <jdepoix@seibert-media.net>
2016-07-20 18:12:52 +0200

    observables for log in and log out added to SessionAuthenticationService

--- app/services/session/session-authentication.service.ts
+++ app/services/session/session-authentication.service.ts
@@ -8,6 +8,12 @@ import {SessionService} from './session.service';
 
 @Injectable()
 export class SessionAuthenticationService {
+  private _onLogInSubject: Subject<void> = new Subject<void>();
+  private _onLogOutSubject: Subject<void> = new Subject<void>();
+
+  public onLogIn: Observable<void> = Observable.from(this._onLogInSubject);
+  public onLogOut: Observable<void> = Observable.from(this._onLogOutSubject);
+
   constructor(private _timetrackerApiService: TimetrackerApiReuqestService, private _sessionService: SessionService) {
   }
 
@@ -19,6 +25,7 @@ export class SessionAuthenticationService {
       password: password
     }).subscribe((plainObject: any) => {
       this._sessionService.authenticationKey = plainObject.token || null;
+      this._onLogInSubject.next(null);
       responseSubject.next(this._sessionService.authenticationKey);
     }, () => {
       this._sessionService.authenticationKey = null;
@@ -32,5 +39,7 @@ export class SessionAuthenticationService {
     this._sessionService.authenticationKey = null;
     this._sessionService.apiUrl = null;
     this._sessionService.selectedProject = null;
+
+    this._onLogOutSubject.next(null);
   }
 }
