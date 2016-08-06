9e761a127849d314fd57bb934b6b340322bd8378
marcelhub <marcelhub2@web.de>
2016-08-06 16:55:30 +0200

    booking implemented, new ETC remaining

--- app/components/bookings/create/create-booking.component.ts
+++ app/components/bookings/create/create-booking.component.ts
@@ -5,7 +5,8 @@ import {BookingService} from "../../../services/bookings/booking.service";
 import {AbstractControl, ControlGroup, FormBuilder, Validators} from "@angular/common";
 import moment = require("moment/moment");
 import {BookingSessionService} from "../../../services/bookings/booking-session.service";
-import {BookingSession} from "../../../models/booking/booking";
+import {BookingSession, Booking} from "../../../models/booking/booking";
+import Moment = moment.Moment;
 
 @Component({
   templateUrl: 'build/components/bookings/create/create-booking.component.html'
@@ -15,16 +16,18 @@ export class CreateBookingComponent {
   effort : AbstractControl;
   description: AbstractControl;
   date: AbstractControl;
+  newEtc: AbstractControl;
   authForm: ControlGroup;
-  minuteValues: Array<Number>=[0,5,10,15,20,25,30,35,40,45,50,55];
+  //minuteValues: Array<Number>=[0,5,10,15,20,25,30,35,40,45,50,55];
   maxDate = moment.utc().startOf('day').format('YYYY-MM-DD');
-  myDate = this.maxDate.toString();
+  myEtc: number;
 
   constructor(navParams: NavParams,
               private _bookingsService: BookingService,
               private _bookingSessionService: BookingSessionService,
               private fb: FormBuilder,
               private nav: NavController) {
+
 
     this._workpackage = navParams.get('workpackage');
     this.authForm = fb.group({
@@ -33,28 +36,28 @@ export class CreateBookingComponent {
       'effort': ['', Validators.required]
     });
 
+    this.newEtc = this.authForm.controls['newEtc'];
     this.effort = this.authForm.controls['effort'];
     this.description = this.authForm.controls['description'];
     this.date = this.authForm.controls['date'];
+    this.myEtc = this._workpackage.etc;
   }
 
   onSubmit() {
     if (this.checkFieldsSet()) {
-      let alert = Alert.create({
-        title: 'Buchung',
-        subTitle: 'Buchung korrekt eingegeben',
-        buttons: ['OK']
+      var booking: Booking = new Booking();
+      booking.date = moment(this.date.value);
+      booking.description = this.description.value;
+      booking.effort = this._stringToWorkday(this.effort.value);
+      booking.workpackage = this._workpackage;
+      this._bookingsService.create(booking).subscribe((returnedBooking: Booking) => {
       });
-      this.nav.present(alert);
     }
   }
 
   checkFieldsSet(): boolean {
-    console.log(this.effort.value);
-    console.log(this.date.value);
     return this.authForm.value.description != '' &&
-          typeof this.effort.value != 'undefined' &&
-          typeof this.date.value != 'undefined';
+          typeof this.effort.value != 'undefined';
   }
 
   createBookingSession(): void {
@@ -63,5 +66,17 @@ export class CreateBookingComponent {
     bookingSession.workpackage = this._workpackage;
     this._bookingSessionService.create(bookingSession).subscribe((returnedBookingSession: BookingSession) => {
     });
+  }
+
+  private _stringToWorkday(effort: string): number {
+    let timeStrings = effort.split(':');
+    let hoursPerDay = parseInt(timeStrings[0])/8;
+    let minutesPerDay = (parseInt(timeStrings[1])/60) /8;
+    return hoursPerDay+minutesPerDay;
+  }
+
+  refreshEtc(): void {
+    this.myEtc = this._workpackage.etc - this._stringToWorkday(this.effort.value);
+    console.log(this.myEtc);
   }
 }
