e5a816fde8d4784d30b2da4c1334d0365ca42820
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-13 23:22:00 +0800

    Delete app directory

--- app/models/project/project.ts
+++
@@ -1,8 +0,0 @@
-import {RestModel} from '../core/rest-model/rest-model';
-
-/**
- * represents a Project or DbIdentifier as it is called in the Database Model.
- */
-export class Project extends RestModel {
-  public db: string;
-}
